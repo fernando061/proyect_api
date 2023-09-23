fa2dd3ea40f140ff5363860779f74a40fbc4942a
ferna <ferna@LAPTOP-KOPV4T9J>
2023-09-22 22:42:51 -0500

    chore: se agregaron roles

---
+++ Core/Entities/RefreshToken.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Entities;
+
+public class RefreshToken
+{
+    public Guid Id { get; set; }
+    public int UsuarioId { get; set; }
+    public User User { get; set; }
+    public string Token { get; set; }
+    public DateTime Expires { get; set; }
+    public bool IsExpired => DateTime.UtcNow >= Expires;
+    public DateTime Created { get; set; }
+    public DateTime? Revoked { get; set; }
+    public bool IsActive => Revoked == null && !IsExpired;
+}
